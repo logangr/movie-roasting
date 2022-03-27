bf30062f8038f3fbe8777620345f0dd63140c57f
Pere Prohens <p8p8g8@gmail.com>
2022-03-27 01:26:25 +0100

    title changed

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,9 +3,9 @@ function Hero() {
   return (
     <div className="py-20 hero">
       <div className="container mx-auto px-6">
-        <h2 className="text-4xl font-bold mb-2 text-white">Adaptam exercici 1</h2>
+        <h2 className="text-4xl font-bold mb-2 text-white">La App para criticar pelis</h2>
         <h3 className="text-2xl mb-8 text-gray-200">
-          Capitulo 1: Explorando vanillajs!
+          A critiar!!!
         </h3>
       </div>
     </div>
